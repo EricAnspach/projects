7daf3d9e5b7b46ecda93bececab840fdccd31fc2
Eric Anspach <eric.j.anspach@gmail.com>
2018-11-22 00:18:04 -0500

    project completion

--- ch07_prj1_ContactList/src/ContactListApp.java
+++ ch07_prj1_ContactList/src/ContactListApp.java
@@ -6,13 +6,31 @@ public class ContactListApp {
 
         String choice = "y";
 
-
         while (choice.equalsIgnoreCase("y")) {
 
+            String firstName = Console.getString("Enter first name: ");
+            String lastName = Console.getString("Enter last name:  ");
+            String email = Console.getString("Enter email:      ");
+            String phone = Console.getString("Enter phone:      ");
 
+            Contact contact = new Contact();
+
+            contact.setFirstName(firstName);
+            contact.setLastName(lastName);
+            contact.setEmail(email);
+            contact.setPhone(phone);
+
+            Console.displayLine();
+            Console.displayLine("------------------------------------------------");
+            Console.displayLine("---- Current Contact ---------------------------");
+            Console.displayLine("------------------------------------------------");
+            Console.displayLine("Name:          " + contact.getFirstName() + " " + contact.getLastName());
+            Console.displayLine("Email Address: " + contact.getEmail());
+            Console.displayLine("Phone number:  " + contact.getPhone());
+            Console.displayLine("------------------------------------------------");
+            Console.displayLine();
             choice = Console.getString("Continue? (y/n): ", "y", "n");
+            Console.displayLine();
         }
-
-
     }
 }
