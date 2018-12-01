382270807f2b610044bbdd86465721c8bf7425d5
Eric Anspach <eric.j.anspach@gmail.com>
2018-12-01 11:00:30 -0500

    chapter 8 project 1

---
+++ ch08_prj1_NumberChecker/src/MyConsole.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class MyConsole extends Console {
+
+    private static Scanner sc = new Scanner(System.in);
+
+    public MyConsole() {
+
+    }
+
+    @Override
+    public String getString(String prompt, String s1, String s2) {
+
+        String s = "";
+        System.out.print(prompt);
+
+        boolean isValid = false;
+        while (!isValid) {
+
+            s = sc.nextLine();
+            if (s.equals("")){
+                System.out.println("Error! This entry is required. Try again.");
+            } else {
+                if(s.equalsIgnoreCase(s1) || s.equalsIgnoreCase(s2)) {
+                    isValid = true;
+                } else {
+                    System.out.println("Error! This entry is required. Try again.");
+                }
+
+            }
+        }
+        return s;
+    }
+}
---
+++ ch08_prj1_NumberChecker/src/NumberCheckerApp.java
@@ -0,0 +1,31 @@
+public class NumberCheckerApp {
+
+
+
+    public static void main(String[] args) {
+
+        MyConsole myConsole = new MyConsole();
+        String choice = "y";
+
+        myConsole.displayLine("Welcome to the Odd/Even Checker!");
+
+        while (choice.equalsIgnoreCase("y")) {
+
+            int number = myConsole.getInt("\nEnter an integer: ");
+
+            checkNumber(number);
+
+            choice = myConsole.getString("Continue? (y/n): ", "y", "n");
+        }
+
+    }
+
+    private static void checkNumber(int number) {
+        if (number % 2 == 0) {
+            System.out.println("The number " + number + " is even.");
+        } else {
+            System.out.println("The number " + number + " is odd.");
+        }
+        System.out.println();
+    }
+}
