6ea5eb7713ea63875b0f719c106528c48afe7924
Eric Anspach <eric.j.anspach@gmail.com>
2019-01-13 23:44:25 -0500

    Created entity classes

---
+++ prs-jpa-demo-1/src/main/java/com/prs/business/PurchaseRequest.java
@@ -0,0 +1,102 @@
+package com.prs.business;
+
+import javax.persistence.*;
+
+@Entity
+public class PurchaseRequest {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private int userId;
+    private String description;
+    private String justification;
+    private String dateNeeded;
+    private String deliveryMode;
+    private String status;
+    private double total;
+    private String submittedDate;
+    private String reasonForRejection;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @ManyToOne
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getJustification() {
+        return justification;
+    }
+
+    public void setJustification(String justification) {
+        this.justification = justification;
+    }
+
+    public String getDateNeeded() {
+        return dateNeeded;
+    }
+
+    public void setDateNeeded(String dateNeeded) {
+        this.dateNeeded = dateNeeded;
+    }
+
+    public String getDeliveryMode() {
+        return deliveryMode;
+    }
+
+    public void setDeliveryMode(String deliveryMode) {
+        this.deliveryMode = deliveryMode;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    public void setTotal(double total) {
+        this.total = total;
+    }
+
+    public String getSubmittedDate() {
+        return submittedDate;
+    }
+
+    public void setSubmittedDate(String submittedDate) {
+        this.submittedDate = submittedDate;
+    }
+
+    public String getReasonForRejection() {
+        return reasonForRejection;
+    }
+
+    public void setReasonForRejection(String reasonForRejection) {
+        this.reasonForRejection = reasonForRejection;
+    }
+}
+
---
+++ prs-jpa-demo-1/src/main/java/com/prs/business/PurchaseRequestLineItem.java
@@ -0,0 +1,48 @@
+package com.prs.business;
+
+import javax.persistence.*;
+
+@Entity
+public class PurchaseRequestLineItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private int purchaseRequestId;
+    private int productId;
+    private int quantity;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @ManyToOne
+    public int getPurchaseRequestId() {
+        return purchaseRequestId;
+    }
+
+    public void setPurchaseRequestId(int purchaseRequestId) {
+        this.purchaseRequestId = purchaseRequestId;
+    }
+
+    @OneToMany(fetch=FetchType.EAGER, cascade=CascadeType.ALL)
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
