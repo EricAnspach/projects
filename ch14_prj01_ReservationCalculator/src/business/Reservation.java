1d635f060fcf99cb79251cf8132c306acb3f0b38
Eric Anspach <eric.j.anspach@gmail.com>
2019-01-23 23:19:24 -0500

    project completion

--- ch14_prj01_ReservationCalculator/src/business/Reservation.java
+++ ch14_prj01_ReservationCalculator/src/business/Reservation.java
@@ -1,6 +1,10 @@
 package business;
 
+import java.text.NumberFormat;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+import java.time.temporal.ChronoUnit;
 
 public class Reservation {
 
@@ -9,13 +13,18 @@ public class Reservation {
 
     final private double NIGHTLYRATE = 145.00;
 
+    public Reservation(LocalDate arrivalDate, LocalDate departureDate) {
+        this.arrivalDate = arrivalDate;
+        this.departureDate = departureDate;
+    }
+
     public LocalDate getArrivalDate() {
         return arrivalDate;
     }
 
-
     public String getArrivalDateFormatted() {
-        return arrivalDateFormatted;
+        DateTimeFormatter dtf = DateTimeFormatter.ofLocalizedDate(FormatStyle.LONG);
+        return dtf.format(getArrivalDate());
     }
 
     public void setArrivalDate(LocalDate arrivalDate) {
@@ -27,25 +36,31 @@ public class Reservation {
     }
 
     public String  getDepartureDateFormatted() {
-        return departureDateFormatted;
+        DateTimeFormatter dtf = DateTimeFormatter.ofLocalizedDate(FormatStyle.LONG);
+        return dtf.format(getDepartureDate());
     }
+
     public void setDepartureDate(LocalDate departureDate) {
         this.departureDate = departureDate;
     }
 
     public int getNumberOfNights() {
-
+        long numberOfNights = ChronoUnit.DAYS.between(arrivalDate,
+                departureDate);
+        return (int) numberOfNights;
     }
 
     public String getPricePerNightFormatted() {
-
+        NumberFormat currency = NumberFormat.getCurrencyInstance();
+        return currency.format(NIGHTLYRATE);
     }
 
     public double getTotalPrice() {
-
+        return NIGHTLYRATE*getNumberOfNights();
     }
 
     public String getTotalPriceFormatted() {
-
+        NumberFormat currency = NumberFormat.getCurrencyInstance();
+        return currency.format(getTotalPrice());
     }
 }
--- ch14_prj01_ReservationCalculator/src/ui/ReservationCalculatorApp.java
+++ ch14_prj01_ReservationCalculator/src/ui/ReservationCalculatorApp.java
@@ -1,6 +1,9 @@
 package ui;
 
+import business.Reservation;
 import util.Console;
+
+import java.time.LocalDate;
 
 public class ReservationCalculatorApp {
 
@@ -14,15 +17,26 @@ public class ReservationCalculatorApp {
 
             int arrivalMonth = Console.getInt("Enter the arrival month (1-12): ", 1, 12);
             int arrivalDay = Console.getInt("Enter the arrival day (1-31): ", 1, 31);
-            int arrivalYear = Console.getInt("Enter the arrival year");
+            int arrivalYear = Console.getInt("Enter the arrival year: ");
 
             int departureMonth = Console.getInt("\nEnter the departure month (1-12): ", 1, 12);
             int departureDay = Console.getInt("Enter the departure day (1-31): ", 1, 31);
-            int departureYear = Console.getInt("Enter the departure year");
+            int departureYear = Console.getInt("Enter the departure year: ");
+
+            LocalDate arrivalDate = LocalDate.of(arrivalYear, arrivalMonth, arrivalDay);
+            LocalDate departureDate = LocalDate.of(departureYear, departureMonth, departureDay);
+
+            Reservation reservation = new Reservation(arrivalDate, departureDate);
+
+            Console.displayLine("\nArrival Date: " + reservation.getArrivalDateFormatted());
+            Console.displayLine("Departure Date: " + reservation.getDepartureDateFormatted());
+            Console.displayLine("Price: " + reservation.getPricePerNightFormatted() + " per night");
+            Console.displayLine("Total Price: " + reservation.getTotalPriceFormatted()
+                                    + " for " + reservation.getNumberOfNights() + " nights");
 
 
-
-            choice = Console.getString("Continue (y/n): ", "y", "n");
+            choice = Console.getString("\nContinue (y/n): ", "y", "n");
+            Console.displayLine();
         }
 
         Console.displayLine("Bye!");
