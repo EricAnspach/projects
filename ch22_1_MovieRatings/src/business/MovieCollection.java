29737c84baa92bbfd539ae5fe056a8a02ab3a58d
Eric Anspach <eric.j.anspach@gmail.com>
2019-02-08 23:53:11 -0500

    Project completion

--- ch22_1_MovieRatings/src/business/MovieCollection.java
+++ ch22_1_MovieRatings/src/business/MovieCollection.java
@@ -1,24 +1,57 @@
 package business;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class MovieCollection {
-    private List<Movie> movies;
+    private List<Movie> movieList;
 
-    public MovieCollection(List<Movie> movies) {
-        this.movies = movies;
+    public MovieCollection() {
+        movieList = new ArrayList<>();
+    }
+
+    public MovieCollection(ArrayList<Movie> movieList) {
+        this.movieList = movieList;
     }
 
     public List<Movie> getMovies() {
-        return movies;
+        return movieList;
     }
 
-    public void setMovies(List<Movie> movies) {
-        this.movies = movies;
+    public void add(Movie movie) {
+        movieList.add(movie);
     }
 
-    public void add() {
-
+    public List<Movie> filterMovies(Predicate<Movie> condition) {
+        List<Movie> filteredMovies = new ArrayList<>();
+        for (Movie m : movieList) {
+            if (condition.test(m)) {
+                filteredMovies.add(m);
+            }
+        }
+        return filteredMovies;
     }
 
+    public double getLowestRating() {
+        return movieList.stream().map(m -> m.getRating()).reduce(5.0, (a, b) -> Math.min(a, b));
+    }
+
+    public double getHighestRating() {
+        return movieList.stream().map(m -> m.getRating()).reduce(0.0, (a, b) -> Math.max(a, b));
+    }
+
+    public double getAverageRating() {
+        List<Double> ratings = movieList.stream().map(m -> m.getRating()).collect(Collectors.toList());
+        double totalRatings = 0.0;
+        for (double r : ratings) {
+            totalRatings += r;
+        }
+        return totalRatings / ratings.size();
+    }
+
+    public int getSize() {
+        return movieList.size();
+    }
 }
--- ch22_1_MovieRatings/src/ui/MovieRatingsApp.java
+++ ch22_1_MovieRatings/src/ui/MovieRatingsApp.java
@@ -4,6 +4,9 @@ import business.Movie;
 import business.MovieCollection;
 import util.Console;
 
+import java.text.NumberFormat;
+import java.time.LocalDate;
+
 public class MovieRatingsApp {
 
     public static void main(String[] args) {
@@ -16,31 +19,88 @@ public class MovieRatingsApp {
         movies.add(new Movie("Clash of the Titans", 2010, 2.6));
         movies.add(new Movie("Citizen Kane", 1941, 4.9));
 
-        Console.displayLine("Welcome to the Movie Ratings Application");
+        Console.displayLine("Welcome to the Movie Ratings Application\n");
         int option;
 
         do {
             printMenu();
             option = Console.getInt("Choose a menu option: ", 1, 6);
+            Console.displayLine();
 
             switch (option) {
                 case 1:
                     int numberToEnter = Console.getInt("How many movies do you wish to enter? ", 1, 10);
+                    Console.displayLine();
 
-                    // for loop to collect movie data from user input
-                    // and add to list
+                    // for loop to collect movie data from user input and add to list
+                    for (int i = 0; i < numberToEnter; i++) {
+                        Console.displayLine("Movie #" + (i+1));
+                        Console.displayLine("----------");
+                        String title = Console.getString("Enter title: ");
+                        int year = Console.getInt("Enter year: ");
+                        double rating = Console.getDouble("Enter rating between 1 and 5 (decimals OK): ");
+                        Console.displayLine();
+
+                        Movie movie = new Movie(title, year, rating);
+                        movies.add(movie);
+                    }
                     break;
                 case 2:
-                    // View top rated movies
+                    // View top rated movies - use filter to get those with 4.0 and higher rating
+                    Console.displayLine("Movies rated 4.0 or higher");
+                    Console.displayLine("--------------------------");
+
+                    movies.getMovies().stream()
+                            .filter(m -> m.getRating() >= 4.0)
+                            .forEach(m -> System.out.println(
+                                    m.getTitle() + " (" +
+                                    m.getYear() + ") Rating: " +
+                                    m.getRating()));
+                    Console.displayLine();
                     break;
                 case 3:
                     // View most recent movies
+                    Console.displayLine("Movies from last 10 years");
+                    Console.displayLine("-------------------------");
+
+                    LocalDate currentDate = LocalDate.now();
+                    int tenYearsAgo = currentDate.getYear() - 10;
+
+                    movies.getMovies().stream()
+                            .filter(m -> m.getYear() > tenYearsAgo)
+                            .forEach(m -> System.out.println(
+                                    m.getTitle() + " (" +
+                                            m.getYear() + ") Rating: " +
+                                            m.getRating()));
+                    Console.displayLine();
                     break;
                 case 4:
-                    // View all movies
+                    // View all movies - enhanced for loop to view all movies
+                    Console.displayLine("All movies");
+                    Console.displayLine("--------------");
+
+                    movies.getMovies().stream()
+                            .forEach(m -> System.out.println(
+                                    m.getTitle() + " (" +
+                                            m.getYear() + ") Rating: " +
+                                            m.getRating()));
+                    Console.displayLine();
                     break;
                 case 5:
-                    // View ratings
+                    // View ratings - use map and reduce to get the movie ratings
+                    // List<Movie> allMovies = movies.getMovies();
+
+                    Console.displayLine("Movie rating data");
+                    Console.displayLine("-----------------");
+                    Console.displayLine("Number of movies: " + movies.getSize());
+                    Console.displayLine("Lowest Rating:    " + movies.getLowestRating());
+                    Console.displayLine("Highest Rating:   " + movies.getHighestRating());
+
+                    NumberFormat number = NumberFormat.getNumberInstance();
+                    number.setMaximumFractionDigits(1);
+                    String avgString = number.format(movies.getAverageRating());
+                    Console.displayLine("Average rating:   " + avgString);
+                    Console.displayLine();
                     break;
                 default:
                     break;
@@ -52,7 +112,7 @@ public class MovieRatingsApp {
     }
 
     private static void printMenu() {
-        Console.displayLine("\n-----------------------");
+        Console.displayLine("-----------------------");
         Console.displayLine("What do you want to do?");
         Console.displayLine("-----------------------");
         Console.displayLine("1 - Enter a movie");
